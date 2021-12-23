bb44ac84409d20e7accb6689239497d4b1ea5b5c
Mahefa Abel <mahefa.razafinirina@pulse.mg>
2021-12-23 08:38:47 +0300

    Update

--- src/app/certificate/list/list.component.ts
+++ src/app/certificate/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewEncapsulation } from '@angular/core';
 import { of } from 'rxjs';
 import { Users } from 'src/models/Users';
 
@@ -7,20 +7,29 @@ import { Users } from 'src/models/Users';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
   // @Input()
   // public listPerson: Array<Users>;
   @Input()
   public isListHasElement!: boolean;
+  @Input()
+  public inputTest!: string;
   public personSelected: Users|null;
   public classItem: Array<string> = []
 
   constructor() {
     // this.listPerson = [];
     this.personSelected = null;
+    console.log("constructor", this.inputTest);
   }
 
   ngOnInit(): void {
+    console.log("ngOnInit", this.inputTest);
+  }
+
+  
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy")
   }
 
 }
--- src/app/certificate/search/search.component.ts
+++ src/app/certificate/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren, ViewEncapsulation } from '@angular/core';
 import { Users } from 'src/models/Users';
 import { DetailComponent } from '../detail/detail.component';
 import { listPersonWithImageJson } from './data.users';
@@ -8,34 +8,42 @@ import { listPersonWithImageJson } from './data.users';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
   public codeCertificate: string;
   public listPersonnResult: Array<Users>;
   public personSelected: Users|null;
   @ViewChild("code") 
   public codeViewChild!: ElementRef
-  // @ViewChild(DetailComponent) 
-  // public detailCompentViewChild!: DetailComponent
-  @ViewChildren(DetailComponent) 
-  public detailCompentViewChildren!: QueryList<DetailComponent>
+  @ViewChild(DetailComponent) 
+  public detailCompentViewChild!: DetailComponent
+  // @ViewChildren(DetailComponent) 
+  // public detailCompentViewChildren!: QueryList<DetailComponent>
 
   constructor() {
     this.codeCertificate = "";
     this.listPersonnResult = [];
     this.personSelected = null
+    console.log("constructor", this.detailCompentViewChild)
   }
   ngOnInit(): void {
+    console.log("ngOnInit", this.detailCompentViewChild)
+  }
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit", this.detailCompentViewChild)
+  }
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy")
   }
 
   onClick(){
     this.listPersonnResult = this.search(this.codeCertificate);
     if(this.listPersonnResult.length == 1) this.personSelected = this.listPersonnResult[0]
     this.codeViewChild.nativeElement.innerHTML = this.codeCertificate;
-    // this.detailCompentViewChild.componentNameViewChild.nativeElement.innerHTML = this.codeCertificate
-    this.detailCompentViewChildren.toArray().forEach( (item: DetailComponent) => {
-        item.componentNameViewChild.nativeElement.innerHTML += this.codeCertificate
-      }
-    )
+    this.detailCompentViewChild.componentNameViewChild.nativeElement.innerHTML = this.codeCertificate
+    // this.detailCompentViewChildren.toArray().forEach( (item: DetailComponent) => {
+    //     item.componentNameViewChild.nativeElement.innerHTML += this.codeCertificate
+    //   }
+    // )
   }
 
   search(codeCertificate: string = ""): Array<Users> {
